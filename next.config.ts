ea2a8bfe6048c85a2c1f4098bc9156d45be21719
Nexusrex18 <lavisnj350@gmail.com>
2026-01-07 21:49:39 +0530

    add hustle

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://btabc.netlify.app', // or simply remove the DENY header
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://btabc.netlify.app",
-          },
-        ],
-      },
-    ]
-  },
+
 };
 
 export default nextConfig;
